4f1fd3292ffaee30565f3a895eec46ade01ec519
Simone-shalom <studzinskis46@gmail.com>
2024-02-21 18:29:59 +0100

    feat:Setup succesfully db connection with pdo- db created

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
+
+$pdo = new PDO("mysql:host=localhost;port=3306;dbname=products-crud","root","");
+
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$statement = $pdo-> prepare('SELECT * from products ORDER BY create_date DESC');
+$statement ->execute();
+$products = $statement-> fetchAll(PDO::FETCH_ASSOC);
+
+print_r($products);
+
 ?>
 
 <!doctype html>
