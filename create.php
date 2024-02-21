5df80f7f1e9649b24ec9f5c58b76b37d333967d0
Simone-shalom <studzinskis46@gmail.com>
2024-02-21 19:16:47 +0100

    feat:Added form for creating products

--- create.php
+++ create.php
@@ -1,5 +1,4 @@
 <?php 
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,6 +10,26 @@
     <title>Document</title>
 </head>
 <body>
-
+<form>
+  <h1>Create a new Product</h1>
+  <div class="mb-3">
+    <label for="prd" class="form-label">Product Title</label>
+    <input type="text" class="form-control" id="prd" aria-describedby="emailHelp">
+    <div id="prd" class="form-text">Add name to a new product</div>
+  </div>
+  <div class="mb-3">
+    <label for="desc" class="form-label">Description</label>
+    <input type="text" class="form-control" id="desc">
+  </div>
+  <div class="mb-3">
+    <label for="img" class="form-label">Image</label>
+    <input type='file' class="form-control" id="img">
+  </div>
+   <div class="mb-3">
+    <label for="price" class="form-label">Price</label>
+    <input type="number" step="0.1" class="form-control" id="price">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
 </body>
 </html>
