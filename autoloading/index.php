3e315f13171fe144a0697581b664a5b9f139991a
Simone-shalom <studzinskis46@gmail.com>
2024-02-28 18:28:26 +0100

    feat:Better version/ guzzle install| setup

--- autoloading/index.php
+++ autoloading/index.php
@@ -4,5 +4,12 @@
 
 require_once "vendor/autoload.php";
 
-$email = new app\Email();
-$person = new app\Person();
+// $email = new app\Email();
+// $person = new app\Person();
+
+$client = new \GuzzleHttp\Client();
+$response = $client->request('GET', 'https://api.github.com/repos/guzzle/guzzle');
+
+echo $response->getStatusCode(); // 200
+echo $response->getHeaderLine('content-type'); // 'application/json; charset=utf8'
+echo $response->getBody(); // '{"id": 1420053, "name": "guzzle", ...}'
