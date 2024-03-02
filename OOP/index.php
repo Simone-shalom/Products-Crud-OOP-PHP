7286aad53750771f960c9d41f76d3770a3d1d4ca
Simone-shalom <studzinskis46@gmail.com>
2024-03-02 16:41:38 +0100

    feat:OOP-phpRecap-part2

--- OOP/index.php
+++ OOP/index.php
@@ -1,7 +1,27 @@
 <?php
 
 class Car {
+    // Properties//Fields
     private $id;
     public $color;
     protected $name;
+    public $vehicleType = 'car';
+
+    // Contructor
+    public function __construct($id, $color, $name) {
+        $this->id = $id;
+        $this->color = $color;
+        $this->name = $name;
+    }
+    // function display car
+    public function displayCar(){
+        return''. $this->id .' '. $this->color . ' '. $this->name .'';
+    }
 }
+// create a new instance of a Car class
+$Car01 = new Car(1,"red", "bmw");
+$Car02 = new Car(2,"blue", "toyota");
+$Car03 = new Car(3,"black", "volvo");
+
+// display it
+echo $Car01->displayCar();
