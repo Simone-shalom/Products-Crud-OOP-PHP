bf6ade44682ed1e42ab12c9957a00d08251b5805
Simone-shalom <studzinskis46@gmail.com>
2024-03-09 16:30:39 +0100

    feat:OOP/db part-2/ db class with properties and method

--- OOP/classes/Database.php
+++ OOP/classes/Database.php
@@ -1,5 +1,21 @@
 <?php
 
 class Database {
-    
+    private $host = 'localhost';
+    private $dbname = 'mydatabase';
+    private $dbusername = 'dbusername';
+    private $dbpassword = '';
+
+    protected function connect(){
+        try {
+
+            $pdo = new PDO("mysql:host=" .$this->host . ";dbname=" . $this->dbname,
+                $this->dbusername, $this->dbpassword);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+
+        }catch(Exception $e){
+            die('Connection failed'. $e->getMessage());
+        }
+    }
 }
