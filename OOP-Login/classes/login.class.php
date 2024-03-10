5c8854d6c9e0cfb7edf1f387faa4a9496e7a14d2
Simone-shalom <studzinskis46@gmail.com>
2024-03-10 20:13:48 +0100

    feat:OOP-login-part15/login- checking for password match/ selecting the user

---
+++ OOP-Login/classes/login.class.php
@@ -0,0 +1,33 @@
+<?php
+
+class LoginClass extends Database{
+ 
+    protected function getUser( string $username, string $password ){
+        // select the password
+        $statement= $this->connect()->prepare("SELECT password FROM users WHERE username=:username");
+        $statement->bindValue("username", $username, PDO::PARAM_STR );
+        $statement->execute();
+
+        // check if passwords are same 
+        $passwordHashed = $statement->fetch(PDO::FETCH_ASSOC);
+        $checkPassword = password_verify($password, $passwordHashed[0]['password']); 
+        
+        if( $checkPassword === false ){
+            $statement = null;
+            header("Location: /Products-Crud-OOP/OOP-Login/index.php?error=wrongPassword");
+            exit();
+        }elseif($checkPassword === true){
+              // if password is same, select the user 
+            $statement = $this->connect()->prepare("SELECT * FROM users WHERE username=:username, password=:password");
+            $statement->bindValue("username", $username, PDO::PARAM_STR );
+            $statement->bindValue("password", $password, PDO::PARAM_STR );
+            $statement->execute();
+
+            
+
+        }
+
+
+    }
+    
+}
