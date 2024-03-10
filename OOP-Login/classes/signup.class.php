71f99320e5a5815a5fc3488802ead6d92d5dec80
Simone-shalom <studzinskis46@gmail.com>
2024-03-10 18:43:33 +0100

    feat:OOP-Login/ part10/ setUser method- inserting user into db

--- OOP-Login/classes/signup.class.php
+++ OOP-Login/classes/signup.class.php
@@ -1,7 +1,22 @@
 <?php
 
 class Signup extends Database {
-    protected function userExists($username, $email){
+
+    protected function setUser( string $username, string $email, string $password){
+        // create a statement
+        $statement = $this->connect()->prepare("INSERT INTO users(username, password, email)
+            VALUES (:username, :email, :password)");
+         // hash the password
+        $hashedPassword = password_hash( $password, PASSWORD_DEFAULT );
+        // bind values
+        $statement -> bindValue(':username', $username);
+        $statement -> bindValue(':email', $email);
+        $statement -> bindValue(':password', $hashedPassword);
+        // execute an statement
+        $statement -> execute();
+    }
+
+    protected function userExists( string $username, string $email){
         $statement = $this->connect()->prepare('SELECT username FROM users WHERE username = :username OR email = :email; ');
         $statement -> bindValue(':username', $username);
         $statement -> bindValue(':email', $email);
