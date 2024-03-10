ec532a318f94f031d585cc18125045d1003fae8a
Simone-shalom <studzinskis46@gmail.com>
2024-03-10 20:00:03 +0100

    feat:OOP-login-part14/login class-login method/errors checking

--- OOP-Login/classes/login.controller.php
+++ OOP-Login/classes/login.controller.php
@@ -9,5 +9,25 @@ class LoginController {
         $this->password = $password;
     }
 
+    public function loginUser(){
+         if($this->isEmpty() === false){
+        // Empty inputs
+        header("Location: /Products-Crud-OOP/OOP-Login/index.php?error=emptyinput");
+        exit();
+    }
+        // getUser if no errors
     
+
+    }
+
+     private function isEmpty(){
+        $result=false;
+        if(empty($this->username) || empty($this->password)) {
+            $result = false;
+        }else {
+            $result = true;
+        }
+        return $result;
+    }
+
 }
--- OOP-Login/includes/login.inc.php
+++ OOP-Login/includes/login.inc.php
@@ -6,4 +6,9 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $username = trim($_POST['username']);
     $password = trim($_POST['password']);
 
+    // Instantiate loginUser login class
+    
+
+    // check for errors, login the user and redirect
+
 }
