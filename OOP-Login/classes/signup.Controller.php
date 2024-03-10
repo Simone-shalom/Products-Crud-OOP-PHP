64b6971899a09d78067ed78ab9cebc4ea9023cbb
Simone-shalom <studzinskis46@gmail.com>
2024-03-10 18:36:06 +0100

    feat:OOP-Login/ part9/ signUpUser method-checking for error/use of private methods

--- OOP-Login/classes/signup.Controller.php
+++ OOP-Login/classes/signup.Controller.php
@@ -13,6 +13,45 @@ class SignupController extends Signup {
         $this->email = $email;
     }
 
+    public function signUpUser(){
+        if($this->isEmpty() === false){
+            // Empty inputs
+            header ("Location:" . $_SERVER['DOCUMENT_ROOT'] . '/index/php?error=emptyinput');
+            exit();
+        }
+
+         if($this->isValidUsername() === false){
+            // Invalid username
+            header ("Location:" . $_SERVER['DOCUMENT_ROOT'] . '/index/php?error=username');
+            exit();
+        }
+
+         if($this->isValidEmail() === false){
+            // Invalid email
+            header ("Location:" . $_SERVER['DOCUMENT_ROOT'] . '/index/php?error=email');
+            exit();
+        }
+
+         if($this->isValidPassword() === false){
+            // Invalid password
+            header ("Location:" . $_SERVER['DOCUMENT_ROOT'] . '/index/php?error=password');
+            exit();
+        }
+
+           if($this->isPasswordMatch() === false){
+            // Passwords do not match
+            header ("Location:" . $_SERVER['DOCUMENT_ROOT'] . '/index/php?error=passwordMatch');
+            exit();
+        }
+           if($this->isUserTaken() === false){
+            // User already exists
+            header ("Location:" . $_SERVER['DOCUMENT_ROOT'] . '/index/php?error=userTaken');
+            exit();
+        }
+
+        // if empty with errors create User
+    }
+
     private function isEmpty(){
         $result=false;
         if(empty($this->username) || empty($this->password) 
