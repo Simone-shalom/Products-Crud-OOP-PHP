e3980ee9665cfb006f846734d69a8eb6717fecf2
Simone-shalom <studzinskis46@gmail.com>
2024-03-16 14:26:15 +0100

    feat:Products-Crud/ good version-mvc/ composer setup/init

---
+++ good/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'simon/good',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '510adcb52b0801fedd362a1433af7da7aad6207f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'simon/good' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '510adcb52b0801fedd362a1433af7da7aad6207f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
