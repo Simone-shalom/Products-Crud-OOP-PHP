a479c4f6ffb2305cb57169378ca79a90ea98019f
Simone-shalom <studzinskis46@gmail.com>
2024-03-16 14:30:39 +0100

    feat:Products-Crud/ good version-mvc/ adding ProductController get route

--- good/public/index.php
+++ good/public/index.php
@@ -1,5 +1,11 @@
 <?php
 
+require_once __DIR__ ."/../vendor/autoload.php";
+
+use app\controllers\ProductController;
+use app\Router;
+
 $router = new Router();
 
-$router->get("/", )
+$router->get("/",[ProductController::class, 'index'] );
+
