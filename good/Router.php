ae8c8a738587a90ac1aa4dda0292c390a9355ef1
Simone-shalom <studzinskis46@gmail.com>
2024-03-16 15:03:32 +0100

    feat:Products-Crud/ good version-mvc/ Router setup/rotes

--- good/Router.php
+++ good/Router.php
@@ -3,5 +3,15 @@
 namespace app;
 
 class Router {
-    
+
+    public array $getRoutes = [];
+    public array $postRoutes = [];
+
+    public function get($url, $fn){
+        $this->getRoutes[$url] = $fn;
+    }
+    public function post($url, $fn){
+        $this->postRoutes[$url] = $fn;
+    }
+
 }
