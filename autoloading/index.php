22baf01f23b63ef6398c81f5af71b691df98752c
Simone-shalom <studzinskis46@gmail.com>
2024-04-13 12:34:28 +0200

    feat:Products-Crud-OOP: working with router and routes

--- autoloading/index.php
+++ autoloading/index.php
@@ -14,3 +14,4 @@ $response = $client->request('GET', 'https://api.github.com/repos/guzzle/guzzle'
 echo $response->getStatusCode(); // 200
 echo $response->getHeaderLine('content-type'); // 'application/json; charset=utf8'
 echo $response->getBody(); // '{"id": 1420053, "name": "guzzle", ...}'
+
